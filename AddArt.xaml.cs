cb8465666a232dc61a63bf45ea5416228f90f57f
rt65s <rt65s@SCHOOL_LAPTOP>
2024-06-10 22:22:36 -0700

    Updated AddArt.xaml, AddArt.xaml.cs, and Data.cs

--- AddArt.xaml.cs
+++ AddArt.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Prog122_Final_Milender
         public AddArt()
         {
             InitializeComponent();//Don't delete this and keep at the top of AddArt()
+            FillComboBox();
+            cmbDate.SelectedIndex = cmbDate.Items.Count -1;
 
 
         }//End of AddArt()
@@ -46,7 +49,77 @@ namespace Prog122_Final_Milender
 
         private void btnAddArt_Click(object sender, RoutedEventArgs e)
         {
+            string artName = txtArtName.Text;
+            int date = int.Parse(cmbDate.Text);
+            string artist = txtArtist.Text;
+            string information = runInformation.Text;
+            Art.Styles style = GetStyle();
+            string filePath = txtFilePath.Text;
+            if(artName.Length > 0 && artist.Length > 0 && information.Length > 0 && filePath.Length > 0)
+            {
+                Art newArt = new Art(artName,date,artist,information,filePath,style);
+                Data.AddArt(newArt);
+                txtArtName.Text = "";
+                txtArtist.Text = "";
+                runInformation.Text = "";
+                txtFilePath.Text = "";
+                rbCubism.IsChecked = true;
+                imgArtWork.Source = null;
+            }
+            else
+            {
+                MessageBox.Show("Make sure all information is filled out");
+            }
+        }
 
+        public Art.Styles GetStyle()
+        {
+            if (rbRealism.IsChecked == true)
+            {
+                return Art.Styles.Realism;
+            }
+            else if (rbSurrealism.IsChecked == true)
+            {
+                return Art.Styles.Surrealism;
+            }
+            else if (rbFresco.IsChecked == true)
+            {
+                return Art.Styles.Fresco;
+            }
+            else if(rbGothic.IsChecked == true)
+            {
+                return Art.Styles.Gothic;
+            }
+            else if(rbFauvism.IsChecked == true)
+            {
+                return Art.Styles.Fauvism;
+            }
+            return Art.Styles.Cubism;
+        }
+
+        public void FillComboBox()
+        {
+
+            for (int i = 1000; i < 2025; i++)
+            {
+                cmbDate.Items.Add(i);
+            }
+        }
+
+        private void btnImagePath_Click(object sender, RoutedEventArgs e)
+        {
+            //Create an instance of OpenFileDialog and name is filePicker
+            OpenFileDialog filePicker = new OpenFileDialog();
+            //Filter by format
+            filePicker.Filter = "Image (*.png, *.jpeg, *.jpg, *.webp)|*.png;*.jpeg;*.jpg; *webp";
+            //Test if user picked a file and said ok with if statement
+            if (filePicker.ShowDialog() == true)
+            {
+                string filePath = filePicker.FileName;
+                txtFilePath.Text = filePath;
+                BitmapImage newImage = Art.GenerateBitMap(filePath);
+                imgArtWork.Source = newImage;
+            }
         }
     }//End class 
 }//End namespace

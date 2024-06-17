b7faf384f06c0cbce901b0cc8549446504ca6ba3
rt65s <rt65s@SCHOOL_LAPTOP>
2024-06-17 14:28:44 -0700

    Updated comments

--- Art.cs
+++ Art.cs
@@ -47,73 +47,104 @@ namespace Prog122_Final_Milender
         //Methods
         //Create method that returns a BitmapImage and takes a string in its parameters -> to convert filePath string to BitmapImage
         public static BitmapImage GenerateBitMap(string filePath)
-        {
+        { //Create new Uri and pass string variable to it
             Uri convertFilePath = new Uri(filePath);
+            //Create new BitmapImage and pass Uri varaible to it
             BitmapImage bitmap = new BitmapImage(convertFilePath);
+            //Return resulting BitmapImage
             return bitmap;
         }
 
         //Create a method that returns a formatted FlowDocument for a Rich Text Box
         public FlowDocument FormattedArtworkPost()
         {
+            //Create an new FlowDocument
             FlowDocument fullDoc = new FlowDocument();
+            //Add the return from methods that format Art fields to the FlowDocument
             fullDoc.Blocks.Add(Date_Formatted());
             fullDoc.Blocks.Add(Nmae_Formatted());
             fullDoc.Blocks.Add(Artist_Formatted())
 ;           fullDoc.Blocks.Add(Information_Formatted());
             fullDoc.Blocks.Add(Styles_Formatted());
+            //Set font family for for the FlowDocument
             fullDoc.FontFamily = new FontFamily("Arial");
+            //Return the formatted FlowDocument
             return fullDoc;
         }
         //Create a method that returns a formatted Paragraph for date
         private Paragraph Date_Formatted()
         {
+            //Create a new Paragraph
             Paragraph para = new Paragraph();
+            //Creaate a new run and pass the field _date to it, and call the ToString method
             Run run = new Run(_date.ToString());
+            //Set font size, font wieght 
             run.FontSize = 10;
             run.FontWeight = FontWeights.Bold;
+            //Add tbe Run to the Paragraph
             para.Inlines.Add(run);
+            //return the formatted paragraph
             return para;
         }
         //Create a method that returns a formatted Paragraph for name
         private Paragraph Nmae_Formatted()
         {
+            //Create a new Paragraph
             Paragraph para = new Paragraph();
+            //Creaate a new run and pass the field _name to it
             Run run = new Run(_name);
+            //Set font size, font wieght
             run.FontSize = 26;
             run.FontWeight = FontWeights.Bold;
+            //Add the Run to the Paragraph
             para.Inlines.Add(run);
+            //Return formatted Paragraph
             return para;
         }
         //Create a method that returns a formatted Peragraph for artist
         private Paragraph Artist_Formatted()
         {
+            //Create a new Paragraph
             Paragraph para = new Paragraph();
+            //Creaate a new run and pass the field _artist to it
             Run run = new Run(_artist);
+            //Set font size, font style and font wieght
             run.FontSize = 18;
             run.FontStyle = FontStyles.Oblique;
             run.FontWeight = FontWeights.Bold;
+            //Add the Run to the Paragraph
             para.Inlines.Add(run);
+            //Return formatted Paragraph
             return para;
         }
         //Create a method that returns a formatted Paragraph for information
         private Paragraph Information_Formatted()
         {
+            //Create a new Paragraph
             Paragraph para = new Paragraph();
+            //Creaate a new run and pass the field _information to it
             Run run = new Run(_information);
+            //Set font size, font style
             run.FontSize = 12;
             run.FontStyle = FontStyles.Italic;
+            //Add the Run to the Paragraph
             para.Inlines.Add(run);
+            //Return formatted Paragraph
             return para;
         }
         //Create a method that returns a formatted Paragraph for styles
         private Paragraph Styles_Formatted()
         {
+            //Create a new Paragraph
             Paragraph para = new Paragraph();
+            //Creaate a new run and pass the field _style to it, and call the ToString method
             Run run = new Run(_styles.ToString());
+            //set font size and text decoration 
             run.FontSize = 16;
             run.TextDecorations = TextDecorations.Underline;
+            //Add the Run to the Paragraph
             para.Inlines.Add(run);
+            //Return formatted Paragraph
             return para;
         }
 
--- Data.cs
+++ Data.cs
@@ -10,21 +10,24 @@ namespace Prog122_Final_Milender
     public static class Data
     {
         //Fields
+        //Create a new instance of ObservableCollection and name it _artwork
         static ObservableCollection<Art> _artworks;
 
         //Constructor
         static Data()
         {
+            //Initialize the ObservableCollection
             _artworks = new ObservableCollection<Art>(); 
-            //Create test Art and add it to the ObservableCollection
-            Art newArt = new Art("Grade Book", 2020, "Walter Waterman", "Class room situation", "C:\\Users\\rt65s\\Documents\\Software II\\Week 11\\Prog122_Final_Milender\\Images\\ReportCard_2.jpeg", Art.Styles.Realism);
-            AddArt(newArt);
+            //Create test Art and add it to the ObservableCollection using the AddArt method
+            //Art newArt = new Art("Grade Book", 2020, "Walter Waterman", "Class room situation", "C:\\Users\\rt65s\\Documents\\Software II\\Week 11\\Prog122_Final_Milender\\Images\\ReportCard_2.jpeg", Art.Styles.Realism);
+            //AddArt(newArt);
         }
 
         //Properties
         public static ObservableCollection<Art> Artworks { get => _artworks; }
 
         //Methods
+        //Create method to Add Art to the ObservableCollection
         public static void AddArt(Art art)
         {
             _artworks.Add(art);
